d806f0e65f5450143eaf18db853bd56507eafe47
Tian <guotian@mail.com>
2020-08-08 11:59:30 +0800

    myservice jar .so

---
+++ MyService/app/src/main/java/com/permission/myservice/MainActivity.java
@@ -0,0 +1,20 @@
+package com.permission.myservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.permission.myjni.TestJNI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TestJNI.pushIntoJNI();
+    }
+
+
+}
